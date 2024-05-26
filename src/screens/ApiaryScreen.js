1a2c651493e33cc9efc8747cc52fe9ad3b811f26
Sventrip86 <fab.sebis@gmail.com>
2024-05-26 09:30:34 +0200

    added ActivityIndicator to be rendered if the page is loading (while fetching apiaries)

--- src/screens/ApiaryScreen.js
+++ src/screens/ApiaryScreen.js
@@ -1,53 +1,73 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import { Button, Icon } from 'react-native-paper';
-import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import { collection, query, where, getDocs, deleteDoc, doc } from 'firebase/firestore';
-import { FIREBASE_AUTH } from '../config/firebaseConfig';
-import { FIRESTORE_DB } from '../config/firebaseConfig';
-import { List, Dialog, Portal, Paragraph, Text, IconButton, Menu, Modal } from 'react-native-paper';
-import { useSelectedApiary } from '../components/SelectedApiaryContex'
-import * as Linking from 'expo-linking';
-
-
-
-
+import React, { useState, useEffect, useCallback } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { Button, Icon } from "react-native-paper";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { FIREBASE_AUTH } from "../config/firebaseConfig";
+import { FIRESTORE_DB } from "../config/firebaseConfig";
+import {
+  List,
+  Dialog,
+  Portal,
+  Paragraph,
+  Text,
+  IconButton,
+  Menu,
+  Modal,
+} from "react-native-paper";
+import { useSelectedApiary } from "../components/SelectedApiaryContex";
+import * as Linking from "expo-linking";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 
 const ApiaryScreen = () => {
   const navigation = useNavigation();
   const [apiaries, setApiaries] = useState([]);
-  const [isDialogVisible, setIsDialogVisible] = useState(false)
-  const [selectedApiary, setSelectedApiary] = useState(false)
-  const [isDialogSelectApiaryVisible, setDialogSelectApiaryVisible] = useState(false)
+  const [isDialogVisible, setIsDialogVisible] = useState(false);
+  const [selectedApiary, setSelectedApiary] = useState(false);
+  const [isDialogSelectApiaryVisible, setDialogSelectApiaryVisible] =
+    useState(false);
   const [isApiarySelected, setIsApiarySelected] = useState(false);
   const [apiariesAvailable, setApiariesAvailable] = useState(false);
   const [menuVisible, setMenuVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-
-
-
-  const toggleMenu = (apiaryId) => setMenuVisible(prev => ({ ...prev, [apiaryId]: !prev[apiaryId] }));
-
+  const toggleMenu = (apiaryId) =>
+    setMenuVisible((prev) => ({ ...prev, [apiaryId]: !prev[apiaryId] }));
 
   const fetchApiaries = async () => {
-    const q = query(collection(FIRESTORE_DB, "apiaries"), where("userId", "==", FIREBASE_AUTH.currentUser.uid));
-    const querySnapshot = await getDocs(q);
-    const apiariesData = [];
-    querySnapshot.forEach((doc) => {
-      apiariesData.push({ id: doc.id, ...doc.data() });
-    });
-    setApiaries(apiariesData);
-    console.log(apiariesData)
+    setIsLoading(true);
+    try {
+      const q = query(
+        collection(FIRESTORE_DB, "apiaries"),
+        where("userId", "==", FIREBASE_AUTH.currentUser.uid)
+      );
+      const querySnapshot = await getDocs(q);
+      const apiariesData = [];
+      querySnapshot.forEach((doc) => {
+        apiariesData.push({ id: doc.id, ...doc.data() });
+      });
+      setApiaries(apiariesData);
+      console.log(apiariesData);
 
+      // Show dialog if no apiaries are found
 
-    // Show dialog if no apiaries are found
-
-    if (apiariesData.length === 0) {
-      setIsDialogVisible(true);
-      setApiariesAvailable(false);
-
-    } else {
-      setApiariesAvailable(true);
+      if (apiariesData.length === 0) {
+        setIsDialogVisible(true);
+        setApiariesAvailable(false);
+      } else {
+        setApiariesAvailable(true);
+      }
+    } catch (error) {
+      console.error("Error fetching apiaries: ", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -59,17 +79,14 @@ const ApiaryScreen = () => {
 
   const { setSelectedApiaryId } = useSelectedApiary();
 
-
-
-
   const handleDialogDismiss = () => {
     setIsDialogVisible(false);
-    navigation.navigate('ApiaryCreationScreen'); // Navigate to creation screen
+    navigation.navigate("ApiaryCreationScreen"); // Navigate to creation screen
   };
 
   const handleDialogSelectApiary = () => {
-    setDialogSelectApiaryVisible(false)
-  }
+    setDialogSelectApiaryVisible(false);
+  };
 
   const handleSelectApiary = (apiaryId) => {
     setSelectedApiaryId(apiaryId);
@@ -77,46 +94,34 @@ const ApiaryScreen = () => {
     setSelectedApiary(apiaryId);
     setIsApiarySelected(true);
 
-    navigation.navigate('HivesTab', { screen: 'Arnie', params: { apiaryId } });
-  }
+    navigation.navigate("HivesTab", { screen: "Arnie", params: { apiaryId } });
+  };
 
   const handleDeleteApiary = async (apiaryId) => {
     try {
       // First, delete all hives within the apiary
-      const hivesRef = collection(FIRESTORE_DB, 'apiaries', apiaryId, 'hives');
+      const hivesRef = collection(FIRESTORE_DB, "apiaries", apiaryId, "hives");
       const querySnapshot = await getDocs(hivesRef);
       await Promise.all(querySnapshot.docs.map((doc) => deleteDoc(doc.ref)));
 
       // Then, delete the apiary document
-      await deleteDoc(doc(FIRESTORE_DB, 'apiaries', apiaryId));
+      await deleteDoc(doc(FIRESTORE_DB, "apiaries", apiaryId));
 
-      await fetchApiaries();  // refresh the list 
+      await fetchApiaries(); // refresh the list
       // display dialog select apiary check
       if (apiaries.length === 0) {
         setIsDialogVisible(true); // No apiaries left, show dialog to create new one
       } // elseif the user delete the selected apiary logic TODO
-
     } catch (error) {
-      console.error('Error deleting apiary: ', error);
+      console.error("Error deleting apiary: ", error);
     }
   };
 
-        //TODO implement logic for updating the apiary
-  const handleUpdateApiary = (apiaryId) => {
-
-  }
+  //************** TODO implement logic for updating the apiary
+  const handleUpdateApiary = (apiaryId) => {};
 
 
-  // function parsePosition(jsonString) {
-  //   try {
-  //     return JSON.parse(jsonString);
-  //   } catch (error) {
-  //     console.error('Failed to parse position JSON', error);
-  //     return null; // or return a default object
-  //   }
-  // }
-
-// ******Open in Google  TODO
+  // ****** Open in Google Maps TODO
 
   // Show modal when the screen first loads and apiaries are available but no selected
   useEffect(() => {
@@ -125,113 +130,125 @@ const ApiaryScreen = () => {
     }
   }, [apiariesAvailable, selectedApiary]);
 
+
+  
+
   return (
     <View style={styles.container}>
-      <Text variant="headlineMedium" style={styles.title}>
-        I tuoi apiari{" "}
-      </Text>
-      <ScrollView contentContainerStyle={styles.contentContainer}>
-        {apiaries.map((apiary) => (
-          <List.Item
-            key={apiary.id}
-            title={apiary.name}
-            description={`Posizione: ${apiary.location} Creato il ${apiary.creationDateString}`}
-            // left={(props) => <List.Icon {...props} icon="bee-flower" />}
-            onPress={() => handleSelectApiary(apiary.id)}
-            style={
-              selectedApiary === apiary.id
-                ? styles.selectedItem
-                : styles.listItem
-            }
-            right={() => (
-              
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                 {/* <Button 
+      {/* if its fetching display the loading indicator otherwise display the list of apiaries  */}
+      {isLoading ? (
+        <ActivityIndicator
+          animating={true}
+          color={MD2Colors.green700}
+          size="large"
+        />
+      ) : (
+        <>
+          <Text variant="headlineMedium" style={styles.title}>
+            I tuoi apiari{" "}
+          </Text>
+          <ScrollView contentContainerStyle={styles.contentContainer}>
+            {apiaries.map((apiary) => (
+              <List.Item
+                key={apiary.id}
+                title={apiary.name}
+                description={`Posizione: ${apiary.location} Creato il ${apiary.creationDateString}`}
+                // left={(props) => <List.Icon {...props} icon="bee-flower" />}
+                onPress={() => handleSelectApiary(apiary.id)}
+                style={
+                  selectedApiary === apiary.id
+                    ? styles.selectedItem
+                    : styles.listItem
+                }
+                right={() => (
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    {/* <Button 
           onPress={() => null}
           mode="outlined">Google Maps</Button>       */}
-                <Menu
-                  visible={menuVisible[apiary.id]}
-                  onDismiss={() => toggleMenu(apiary.id)}
-                  anchor={
-                    <IconButton
-                      icon="dots-vertical"
-                      onPress={() => toggleMenu(apiary.id)}
-                    />
-                  }
-                >
-                  <Menu.Item
-                    leadingIcon="pencil"
-                    onPress={() => handleUpdateApiary(apiary.id)}
-                    title="Modifica"
-                  />
-                  <Menu.Item
-                    leadingIcon="delete"
-                    onPress={() => handleDeleteApiary(apiary.id)}
-                    title="Elimina"
-                  />
-                </Menu>
-                 
-              </View>
-            )}
-          />
-        ))}
-        <Button
-          mode="contained"
-          title="Create apiary"
-          onPress={() => {
-            navigation.navigate("ApiaryCreationScreen");
-          }}
-        >
-          Crea Apiario
-        </Button>
-      </ScrollView>
+                    <Menu
+                      visible={menuVisible[apiary.id]}
+                      onDismiss={() => toggleMenu(apiary.id)}
+                      anchor={
+                        <IconButton
+                          icon="dots-vertical"
+                          onPress={() => toggleMenu(apiary.id)}
+                        />
+                      }
+                    >
+                      <Menu.Item
+                        leadingIcon="pencil"
+                        onPress={() => handleUpdateApiary(apiary.id)}
+                        title="Modifica"
+                      />
+                      <Menu.Item
+                        leadingIcon="delete"
+                        onPress={() => handleDeleteApiary(apiary.id)}
+                        title="Elimina"
+                      />
+                    </Menu>
+                  </View>
+                )}
+              />
+            ))}
+            <Button
+              mode="contained"
+              title="Create apiary"
+              onPress={() => {
+                navigation.navigate("ApiaryCreationScreen");
+              }}
+            >
+              Crea Apiario
+            </Button>
+          </ScrollView>
 
-      {/**************    MODALS    ***********/}
+          {/**************    MODALS    ***********/}
 
-      <Modal
-        visible={isDialogVisible}
-        onDismiss={handleDialogSelectApiary}
-        contentContainerStyle={styles.modalContent}
-      >
-        <View style={styles.modalInnerContent}>
-          <IconButton icon="dots-vertical" style={styles.iconStyle} />
-          <Text style={styles.textStyle}>
-            Nessun apiario presente. Non esistono apiari, creane subito uno!
-          </Text>
-          <Button
-            mode="contained"
-            onPress={handleDialogDismiss}
-            style={styles.buttonStyle}
+          <Modal
+            visible={isDialogVisible}
+            onDismiss={handleDialogSelectApiary}
+            contentContainerStyle={styles.modalContent}
           >
-            OK
-          </Button>
-        </View>
-      </Modal>
+            <View style={styles.modalInnerContent}>
+              <IconButton icon="dots-vertical" style={styles.iconStyle} />
+              <Text style={styles.textStyle}>
+                Nessun apiario presente. Non esistono apiari, creane subito uno!
+              </Text>
+              <Button
+                mode="contained"
+                onPress={handleDialogDismiss}
+                style={styles.buttonStyle}
+              >
+                OK
+              </Button>
+            </View>
+          </Modal>
 
-      <Modal
-        visible={isDialogSelectApiaryVisible}
-        onDismiss={handleDialogSelectApiary}
-        contentContainerStyle={styles.modalContent}
-      >
-        <View style={styles.modalInnerContent}>
-          <IconButton
-            icon="alert-box"
-            size={50}
-            iconColor="#F71735"
-            style={styles.iconStyle}
-          />
-          <Text style={styles.textStyle}>
-            Nessun apiario selezionato! Seleziona un apiario dalla lista
-          </Text>
-          <Button
-            mode="contained"
-            onPress={handleDialogSelectApiary}
-            style={styles.buttonStyle}
+          <Modal
+            visible={isDialogSelectApiaryVisible}
+            onDismiss={handleDialogSelectApiary}
+            contentContainerStyle={styles.modalContent}
           >
-            OK
-          </Button>
-        </View>
-      </Modal>
+            <View style={styles.modalInnerContent}>
+              <IconButton
+                icon="alert-box"
+                size={50}
+                iconColor="#F71735"
+                style={styles.iconStyle}
+              />
+              <Text style={styles.textStyle}>
+                Nessun apiario selezionato! Seleziona un apiario dalla lista
+              </Text>
+              <Button
+                mode="contained"
+                onPress={handleDialogSelectApiary}
+                style={styles.buttonStyle}
+              >
+                OK
+              </Button>
+            </View>
+          </Modal>
+        </>
+      )}
     </View>
   );
 };
@@ -241,61 +258,59 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   contentContainer: {
     // width: '100%',
-
   },
   text: {
     fontSize: 20,
     marginBottom: 20,
-    textAlign: 'center',
-    fontWeight: 'bold'
+    textAlign: "center",
+    fontWeight: "bold",
   },
   selectedItem: {
-    backgroundColor: '#FFD670', // Light yellow for selected apiary
+    backgroundColor: "#FFD670", // Light yellow for selected apiary
     borderRadius: 8,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     marginVertical: 10,
     marginHorizontal: 10, // Added horizontal margin
   },
   listItem: {
-    backgroundColor: '#fff', // Background color for non-selected apiaries
+    backgroundColor: "#fff", // Background color for non-selected apiaries
     borderRadius: 8,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     marginVertical: 10,
     marginHorizontal: 10,
   },
   modalContent: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 20,
     margin: 20,
     borderRadius: 10,
     elevation: 5,
   },
   modalInnerContent: {
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    alignItems: "center",
+    justifyContent: "space-around",
     height: 200,
   },
   iconStyle: {
     marginBottom: 10,
   },
   textStyle: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 20,
     marginBottom: 15,
-  }, 
-  title: {
-    textAlign: 'center',
-    fontSize: 25,
-    fontWeight: 'bold',
-    marginBottom: 20, 
   },
- 
+  title: {
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
 });
 
 export default ApiaryScreen;
